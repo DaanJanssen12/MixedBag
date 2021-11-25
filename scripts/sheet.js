086b1e63e4ac5329d80695c88c378d00d78b0d60
DaanJanssen12 <Daantjejanssen12@gmail.com>
2021-11-25 20:43:04 +0100

    digital character sheet

--- scripts/sheet.js
+++ scripts/sheet.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   var self = this;
+  var doc = new jsPDF();
   $("input:text.stat").off("keyup").on("keyup", function(){
     var stat = $(this).attr("name");
     var score = $(this).val();
@@ -9,7 +10,44 @@ $(document).ready(function(){
     var name = $(this).attr("name").replace("-prof", "");
     var checked = $(this).is(":checked");
     ToggleProficiency(name, checked);
-  })
+  });
+  $("#download").off("click").on("click", function(){
+    const quality = 10 // Higher the better but larger file
+    var padding = 10;
+    html2canvas(document.querySelector('body'),
+        { scale: quality }
+    ).then(canvas => {
+        // Get x and y coordinates
+        var coordinates = document.getElementsByClassName('charsheet')[0].getBoundingClientRect();
+        var x1 = coordinates.left - padding;
+        var x2 = coordinates.right + padding;
+        var y1 = coordinates.top - padding;
+        var y2 = coordinates.bottom + padding;
+
+        // calc the size -- but no larger than the html2canvas size!
+        var width = Math.min(canvas.width,Math.abs(x2-x1));
+        var height = Math.min(canvas.height,Math.abs(y2-y1));
+
+        // create a new avatarCanvas with the specified size
+        var avatarCanvas = document.createElement('canvas');
+        avatarCanvas.width=width;
+        avatarCanvas.height=height;
+        avatarCanvas.id = 'avatarCanvas';
+
+        // put avatarCanvas into document body
+        document.body.appendChild(avatarCanvas);
+
+        // use the clipping version of drawImage to draw
+        // a clipped portion of html2canvas's canvas onto avatarCanvas
+        var avatarCtx = avatarCanvas.getContext('2d');
+        avatarCtx.drawImage(canvas,x1,y1,width,height,0,0,width,height);
+
+        //Create PDF
+        const pdf = new jsPDF('p', 'mm', 'a4');
+        pdf.addImage(avatarCtx.canvas.toDataURL('image/png'), 'PNG', 0, 0, 210, 297);
+        pdf.save("Test.pdf");
+    });
+  });
 });
 
 function ChangeModifier(stat, score){
@@ -39,4 +77,13 @@ function ToggleProficiency(name, checked){
     val -= profBonus;
   }
   $(`input[name='${name}']`).val(val <= 0 ? val : "+"+val);
+}
+
+function RecalculateSkillProficiencies(){
+  var profBonus = parseInt($("input[name='proficiencybonus']").val().replace("+",""));
+  $("input[name$='prof']").each(function(){
+    if($(this).is(":checked")){
+
+    }
+  });
 }
