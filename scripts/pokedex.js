882bd6b02e48c1a841999d90aef455ab42c89a12
Daan <daantjejanssen12@gmail.com>
2022-08-17 20:33:33 +0200

    doing stuff

--- scripts/pokedex.js
+++ scripts/pokedex.js
@@ -9,7 +9,42 @@ function readTextFile(file, callback) {
     }
     rawFile.send(null);
 }
-let fireOnce = 0;
+function replaceAll(str, find, replace){
+    return str.replace(new RegExp(escapeRegExp(find), 'g'), replace);
+}
+function escapeRegExp(string){
+    return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+}
+function getEffectivenessLabel(amount){
+    switch(amount){
+        case 0.25:
+            return "¼";
+        case 0.5:
+            return "½";
+        case 1:
+            return "";
+        default:
+            return amount;
+    }
+}
+function getStatRank(stat){
+    if(stat <= 30){
+        return 1;
+    } else if (stat <= 60){
+        return 2;
+    } else if (stat <= 60){
+        return 3;
+    } else if (stat <= 90){
+        return 4;
+    } else if (stat <= 120){
+        return 5;
+    } else if (stat <= 150){
+        return 6;
+    } else {
+        return 7;
+    }
+}
+fireOnce = 0;
 
 $(document).ready(function(){
     if(fireOnce <= 0){
@@ -18,15 +53,39 @@ $(document).ready(function(){
         var pokemon = 'data/pokemon/' + id + '.json';
         readTextFile(pokemon, function(text){
             var data = JSON.parse(text);
-            console.log(data);
     
             var html = document.body.innerHTML;
-            html = html.replace("$Name", data.name);
-            html = html.replace("$PrimaryType", data.primaryType);
-            html = html.replace("$PrimaryTypeLabel", data.primaryTypeLabel);
+            
+            html = replaceAll(html, "$Name", data.name);
+            html = replaceAll(html, "$PrimaryTypeLower", data.primaryType.toLowerCase());
+            html = replaceAll(html, "$PrimaryType", data.primaryType);
+            html = replaceAll(html, "$Species", data.species);
+            html = replaceAll(html, "$Height", data.height);
+            html = replaceAll(html, "$Weight", data.weight);
+            html = replaceAll(html, "$Abilities", data.abilities.replace(",", "</br>"));
+            html = replaceAll(html, "$NormalTypeEffectivenessClass", (data.typeDefenceEffectiveness.normal * 100));
+            html = replaceAll(html, "$NormalTypeEffectiveness", getEffectivenessLabel(data.typeDefenceEffectiveness.normal));
 
-            document.body.innerHTML = html; 
+            html = replaceAll(html, "$FireTypeEffectivenessClass", (data.typeDefenceEffectiveness.fire * 100));
+            html = replaceAll(html, "$FireTypeEffectiveness", getEffectivenessLabel(data.typeDefenceEffectiveness.fire));
+
+            html = replaceAll(html, "$WaterTypeEffectivenessClass", (data.typeDefenceEffectiveness.water * 100));
+            html = replaceAll(html, "$WaterTypeEffectiveness", getEffectivenessLabel(data.typeDefenceEffectiveness.water));
+
+            html = replaceAll(html, "$ElectricTypeEffectivenessClass", (data.typeDefenceEffectiveness.electric * 100));
+            html = replaceAll(html, "$ElectricTypeEffectiveness", getEffectivenessLabel(data.typeDefenceEffectiveness.electric));
+
+            html = replaceAll(html, "$GrassTypeEffectivenessClass", (data.typeDefenceEffectiveness.grass * 100));
+            html = replaceAll(html, "$GrassTypeEffectiveness", getEffectivenessLabel(data.typeDefenceEffectiveness.grass));
+
+            html = replaceAll(html, "$AtkStat", data.stats.atk);
+
+            document.body.innerHTML = html;
+            var imgSrc = "data/pokemon/img/"+data.name+".png";
+            $("#POKEMON_IMG").attr("src", imgSrc); 
+            $("#AtkWidthBar")
+                .width(((parseInt(data.stats.atk)/180) * 100) + "%")
+                .addClass("barchart-rank-"+getStatRank(data.stats.atk));
         });
-        $("#POKEMON_IMG").attr("src", "data/pokemon/img/salareen.png");
     }
 });
